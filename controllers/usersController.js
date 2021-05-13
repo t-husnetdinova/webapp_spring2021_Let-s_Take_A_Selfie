3baff6d8205f5d259f37ad3161732135c4db1cfb
Tammy Husnetdinova <43862224+t-husnetdinova@users.noreply.github.com>
2021-05-13 17:19:50 -0600

    added more user routes and sidenav for trending hashtags

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -137,5 +137,77 @@ module.exports = {
         req.flash("success", "Successfully logged out!");
         res.locals.redirect = "/";
         next();
+    },
+    redirectView: (req, res, next) => {
+        let redirectPath = res.locals.redirect;
+        if (redirectPath != undefined) res.redirect(redirectPath);
+        else next();
+    },
+    show: (req, res, next) => {
+        let userId = req.params.id;
+        User.findById(userId)
+            .then(user => {
+                res.locals.user = user;
+                next();
+            })
+            .catch(error => {
+                console.log(`Error fetching user by ID: ${error.message}`);
+                next(error);
+            })
+    },
+    showView: (req, res) => {
+        res.render("users/show")
+    },
+    edit: (req, res, next) => {
+        let userId = req.params.id;
+        User.findById(userId)
+            .then(user => {
+                res.render("users/edit", { user: user });
+            })
+            .catch(error => {
+                console.log(`Error loading user by ID: ${error.message}`);
+                next(error);
+            })
+    },
+    update: (req, res, next) => {
+        if(req.skip) return next();
+
+        let userId = req.params.id;
+        // let updatedUser = new User ({
+        //     firstName: req.body.firstName,
+        //     lastName: req.body.lastName,
+        //     email: req.body.email,
+        //     zipCode: req.body.zipCode,
+        //     password: req.body.password
+        // });
+        var updatedUser = {};
+        updatedUser.firstName = req.body.firstName;
+        updatedUser.lastName =  req.body.lastName;
+        updatedUser.email = req.body.email;
+        updatedUser.zipCode = req.body.zipCode;
+        updatedUser.password = req.body.password;
+        
+        User.findByIdAndUpdate(userId, updatedUser)
+        .then(user => {
+            res.locals.user = user;
+            res.locals.redirect = `/users/${user._id}`;
+            next();
+        })
+        .catch(error => {
+            console.log(`Error loading user by ID: ${error.message}`);
+            next(error);
+        })
+    }, 
+    delete: (req, res, next) => {
+        let userId = req.params.id;
+        User.findByIdAndRemove(userId)
+        .then(() => {
+            res.locals.redirect = "/users";
+            next();
+        })
+        .catch(error => {
+            console.log(`Error loading user by ID: ${error.message}`);
+            next(error);
+        })
     }
 };
