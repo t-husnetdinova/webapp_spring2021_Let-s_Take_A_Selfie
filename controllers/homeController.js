3ea6b98e85cf4cb214bf471d5b3c3801b6528e08
Tammy Husnetdinova <43862224+t-husnetdinova@users.noreply.github.com>
2021-05-13 20:02:36 -0600

    cleaned up unused files and code

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,14 +3,6 @@ module.exports = {
     index: (req, res) => {
         res.render("index");
     },
-    results: (req, res) => {
-        res.render("search");
-    },
-    // settings
-    settings: (req, res) => {
-        res.render("settings");
-    },
-    //account
     account: (req, res) => {
         res.render("account");
     }
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -3,8 +3,6 @@ const router = require("express").Router(),
     homeController = require("../controllers/homeController");
 
 // home routes
-router.get("/search", homeController.results);
-router.get("/settings", homeController.settings);
 router.get("/account", homeController.account);
 
 module.exports = router;
